e28fbdf53432b39eab6ed30434be98ae54d629c7
이중호 <spebz@naver.com>
2024-07-10 13:22:44 +0900

    Movement and Collision

--- Assets/Scripts/Extensions.cs
+++ Assets/Scripts/Extensions.cs
@@ -1,9 +1,9 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public static class Extensions
 {
-    private static LayerMask layerMask =LayerMask.GetMask("Default");
+    private static LayerMask layerMask = LayerMask.GetMask("Default");
+
     public static bool Raycast(this Rigidbody2D rigidbody, Vector2 direction)
     {
         if (rigidbody.isKinematic)
@@ -12,9 +12,15 @@ public static class Extensions
         }
 
         float radius = 0.25f;
-        float distance = 0.125f;
+        float distance = 0.375f;
 
-        RaycastHit2D hit = Physics2D.CircleCast(rigidbody.position, radius, direction, distance, layerMask);
-        return hit.collider != null && hit.rigidbody != rigidbody;    
+        RaycastHit2D hit = Physics2D.CircleCast(rigidbody.position, radius, direction.normalized, distance, layerMask);
+        return hit.collider != null && hit.rigidbody != rigidbody;
+    }
+
+    public static bool DotTest(this Transform origin, Transform target, Vector2 testDirection)
+    {
+        Vector2 direction = target.position - origin.position;
+        return Vector2.Dot(direction.normalized, testDirection) > 0.5f;
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,57 +7,77 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector2 velocity;
     private float inputAxis;
+    private float jumpTimeCounter;
+    private bool isJumping;
 
     public float moveSpeed = 8f;
     public float maxJumpHeight = 5f;
     public float maxJumpTime = 1f;
 
-    public float jumpForce => (2f*maxJumpHeight) / (maxJumpTime/2f);
-    public float gravity => (-2f*maxJumpHeight) / Mathf.Pow((maxJumpTime / 2f), 2);
+    public float jumpForce => (2f * maxJumpHeight) / (maxJumpTime / 2f);
+    public float gravity => (-2f * maxJumpHeight) / Mathf.Pow((maxJumpTime / 2f), 2);
 
     public bool grounded { get; private set; }
-    public bool jumping { get; private set; }
 
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
-        camera=Camera.main;
+        camera = Camera.main;
     }
 
     private void Update()
     {
-        HorizontalMovement();
-
         grounded = rigidbody.Raycast(Vector2.down);
 
-        if(grounded)
+        HorizontalMovement();
+
+        if (grounded)
         {
             GroundedMovement();
         }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            isJumping = false;
+        }
+
         ApplyGravity();
     }
 
     private void HorizontalMovement()
     {
         inputAxis = Input.GetAxis("Horizontal");
-        velocity.x = Mathf.MoveTowards(velocity.x, inputAxis*moveSpeed, moveSpeed*Time.deltaTime);   
+        velocity.x = inputAxis * moveSpeed;
     }
 
     private void GroundedMovement()
     {
         velocity.y = Mathf.Max(velocity.y, 0f);
-        jumping = velocity.y > 0f;
 
         if (Input.GetButtonDown("Jump"))
         {
             velocity.y = jumpForce;
-            jumping = true;
+            jumpTimeCounter = maxJumpTime;
+            isJumping = true;
+        }
+
+        if (Input.GetButton("Jump") && isJumping)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                velocity.y = jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
         }
     }
 
     private void ApplyGravity()
     {
-        bool falling = velocity.y <0f || Input.GetButton("Jump");
+        bool falling = velocity.y < 0f || !isJumping;
         float multiplier = falling ? 2f : 1f;
 
         velocity.y += gravity * multiplier * Time.deltaTime;
@@ -67,12 +87,23 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         Vector2 position = rigidbody.position;
-        position += velocity*Time.fixedDeltaTime;
+        position += velocity * Time.fixedDeltaTime;
 
         Vector2 leftEdge = camera.ScreenToWorldPoint(Vector2.zero);
         Vector2 rightEdge = camera.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
-        position.x = Mathf.Clamp(position.x, leftEdge.x+0.5f, rightEdge.x-0.5f);
+        position.x = Mathf.Clamp(position.x, leftEdge.x + 0.5f, rightEdge.x - 0.5f);
 
         rigidbody.MovePosition(position);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer != LayerMask.NameToLayer("PowerUp"))
+        {
+            if (transform.DotTest(collision.transform, Vector2.up))
+            {
+                velocity.y = 0f;
+            }
+        }
     }
 }
