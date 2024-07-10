bb88fa54b4e1c32cab05fcc10e52bc9d947c560e
wndghh <spebz@naver.com>
2024-07-10 09:26:53 +0900

    Movement added but need adjustment

---
+++ Assets/Scripts/Extensions.cs
@@ -0,0 +1,20 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public static class Extensions
+{
+    private static LayerMask layerMask =LayerMask.GetMask("Default");
+    public static bool Raycast(this Rigidbody2D rigidbody, Vector2 direction)
+    {
+        if (rigidbody.isKinematic)
+        {
+            return false;
+        }
+
+        float radius = 0.25f;
+        float distance = 0.125f;
+
+        RaycastHit2D hit = Physics2D.CircleCast(rigidbody.position, radius, direction, distance, layerMask);
+        return hit.collider != null && hit.rigidbody != rigidbody;    
+    }
+}
